ef0cb8cc75a7582dda0f87637a08c522e39ca052
lucio guerra <lucioguerra2001@gmail.com>
2024-12-27 18:38:23 -0300

    Primer commit

---
+++ requirement-sv/src/main/java/com/tickets/requirement_sv/RequirementSvApplication.java
@@ -0,0 +1,13 @@
+package com.tickets.requirement_sv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequirementSvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequirementSvApplication.class, args);
+	}
+
+}
---
+++ requirement-sv/src/test/java/com/tickets/requirement_sv/RequirementSvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tickets.requirement_sv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequirementSvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
