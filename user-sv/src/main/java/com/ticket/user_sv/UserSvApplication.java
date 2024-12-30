c47905179c619b82444e75ee02157d37d01e70da
zoe.quiroz3 <zoequiroz03@gmail.com>
2024-12-29 22:30:07 -0300

    mi primer commit :)

---
+++ user-sv/src/main/java/com/ticket/user_sv/UserSvApplication.java
@@ -0,0 +1,17 @@
+package com.ticket.user_sv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class UserSvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserSvApplication.class, args);
+	}
+
+}
---
+++ user-sv/src/main/java/com/ticket/user_sv/configuration/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.ticket.user_sv.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtException;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/public/**").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .cors(withDefaults())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()))
+                .sessionManagement((session) -> session.sessionCreationPolicy(STATELESS))
+                .build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return new JwtDecoder() {
+            @Override
+            public Jwt decode(String token) throws JwtException {
+                return null;
+            }
+        };
+    }
+}
