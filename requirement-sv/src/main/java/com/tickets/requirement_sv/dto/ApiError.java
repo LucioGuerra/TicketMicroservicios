552348a20ca995611bce197c52dac368d276c6cf
LucioGuerra <lucioguerra2001@gmail.com>
2025-01-04 18:41:25 -0300

    hot fix en api error

--- requirement-sv/src/main/java/com/tickets/requirement_sv/dto/ApiError.java
+++ requirement-sv/src/main/java/com/tickets/requirement_sv/dto/ApiError.java
@@ -1,4 +1,4 @@
 package com.tickets.requirement_sv.dto;
 
-public record ApiError(String error, String message, Integer status) {
+public record ApiError(String code, String message, Integer status) {
 }
