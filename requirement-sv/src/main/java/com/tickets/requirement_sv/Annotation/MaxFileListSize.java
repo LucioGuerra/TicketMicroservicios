72349a3cfafbd3122f59b5f98609cb15b71f272b
LucioGuerra <lucioguerra2001@gmail.com>
2025-01-09 11:01:45 -0300

    Creacion del Dockerfile y leve correcion de MaxFileListSize

--- requirement-sv/src/main/java/com/tickets/requirement_sv/Annotation/MaxFileListSize.java
+++ requirement-sv/src/main/java/com/tickets/requirement_sv/Annotation/MaxFileListSize.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Constraint(validatedBy = MaxFileListSize.class)
+@Constraint(validatedBy = MaxFileListValidator.class)
 @Target({ElementType.PARAMETER, ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface MaxFileListSize {
