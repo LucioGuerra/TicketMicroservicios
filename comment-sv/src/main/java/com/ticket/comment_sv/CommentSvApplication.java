ad4ee7615b062d2e2a411459e17204082a0d986a
LucioGuerra <lucioguerra2001@gmail.com>
2025-01-10 10:35:38 -0300

    Primer commit de comment-sv

---
+++ comment-sv/src/main/java/com/ticket/comment_sv/CommentSvApplication.java
@@ -0,0 +1,13 @@
+package com.ticket.comment_sv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentSvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentSvApplication.class, args);
+	}
+
+}
