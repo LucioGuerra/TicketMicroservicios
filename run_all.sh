7ddf3f49bf6e609ad8b9c1f7798a61120a128c78
LucioGuerra <lucioguerra2001@gmail.com>
2025-02-26 12:11:38 -0300

    Modificacion del script run

---
+++ run_all.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+set -a
+source .env-build
+set +a
+
+docker-compose up postgres -d
+
+cd Bibliotecas/file-service-library
+mvn clean install
+cd ..
+cd ..
+
+SERVICES=("api-gateway" "comment-sv" "eureka-sv" "requirement-sv" "traceability-sv" "type-sv")
+
+for service in "${SERVICES[@]}"; do
+  echo "Compilando servicio: $service"
+
+   case "$service" in
+      "comment-sv")
+        export DB_NAME="comment_db"
+        ;;
+      "requirement-sv")
+        export DB_NAME="requirement_db"
+        ;;
+      "traceability-sv")
+        export DB_NAME="traceability_db"
+        ;;
+      "type-sv")
+        export DB_NAME="type_db"
+        ;;
+      "user-sv")
+        export DB_NAME="user_db"
+        ;;
+      *)
+        unset DB_NAME  # Para servicios que no necesitan DB_NAME
+        ;;
+    esac
+
+  if [ -d "$service" ]; then
+    cd "$service" || { echo "No se pudo acceder al directorio $service"; exit 1; }
+    mvn clean package || { echo "Error compilando $service"; exit 1; }
+    cd ..
+  else
+    echo "El directorio $service no existe"
+  fi
+done
+
+docker-compose down
+
+docker-commpose up -d
+
+echo "Todos los servicios han sido compilados exitosamente."
