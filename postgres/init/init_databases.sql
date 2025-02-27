053d6892dc377e454455e62e950eca718965fb7b
LucioGuerra <lucioguerra2001@gmail.com>
2025-02-26 21:53:39 -0300

    Configuracion de cors en el api gateway

--- postgres/init/init_databases.sql
+++ postgres/init/init_databases.sql
@@ -38,35 +38,5 @@ WHERE NOT EXISTS (
 \gexec
 
 
-INSERT INTO comment_db.public.comment (
-    created_at,
-    deleted,
-    description,
-    requirement_id,
-    subject,
-    updated_at,
-    user_id
-)
-SELECT
-    NOW() - (interval '1 day' * random()) AS created_at, -- fecha aleatoria en las últimas 24 horas
-    (random() < 0.5) AS deleted, -- booleano aleatorio
-    (ARRAY[
-        'Este es un comentario sobre la funcionalidad del sistema.',
-        'La implementación requiere revisión.',
-        'Se ha detectado un comportamiento inesperado en el módulo de pagos.',
-        'El sistema responde de manera óptima en condiciones de alta carga.',
-        'Es necesario realizar ajustes en la configuración del servidor.'
-        ])[floor(random() * 5)::int + 1] AS description,
-    floor(random() * 100)::int AS requirement_id, -- requirement_id aleatorio entre 0 y 99
-    (ARRAY[
-        'Revisión de sistema',
-        'Ajuste requerido',
-        'Notificación de error',
-        'Alerta de rendimiento',
-        'Observación técnica'
-        ])[floor(random() * 5)::int + 1] AS subject,
-    NOW() - (interval '1 day' * random()) AS updated_at, -- fecha aleatoria en las últimas 24 horas
-    floor(random() * 50)::int AS user_id -- user_id aleatorio entre 0 y 49
-FROM generate_series(1, 30);
 
 
