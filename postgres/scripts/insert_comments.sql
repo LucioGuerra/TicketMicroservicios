7821a6b1d2109098a6ceb5926e50500545f3ab56
LucioGuerra <lucioguerra2001@gmail.com>
2025-03-05 09:02:47 -0300

    Hot fix en obtener los comentarios de un requerimiento

--- postgres/scripts/insert_comments.sql
+++ postgres/scripts/insert_comments.sql
@@ -7,13 +7,13 @@ INSERT INTO comment_db.public.comment (
     updated_at, 
     user_id
 ) VALUES 
-    (NOW(), FALSE, 'Comentario de prueba 1', 1, 'Asunto 1', NOW(), 101),
-    (NOW(), FALSE, 'Comentario de prueba 2', 2, 'Asunto 2', NOW(), 102),
-    (NOW(), FALSE, 'Comentario de prueba 3', 1, 'Asunto 3', NOW(), 103),
-    (NOW(), FALSE, 'Comentario de prueba 4', 3, 'Asunto 4', NOW(), 104),
-    (NOW(), FALSE, 'Comentario de prueba 5', 2, 'Asunto 5', NOW(), 105),
-    (NOW(), FALSE, 'Comentario de prueba 6', 1, 'Asunto 6', NOW(), 106),
-    (NOW(), FALSE, 'Comentario de prueba 7', 3, 'Asunto 7', NOW(), 107),
-    (NOW(), FALSE, 'Comentario de prueba 8', 2, 'Asunto 8', NOW(), 108),
-    (NOW(), FALSE, 'Comentario de prueba 9', 1, 'Asunto 9', NOW(), 109),
-    (NOW(), FALSE, 'Comentario de prueba 10', 3, 'Asunto 10', NOW(), 110);
+    (NOW(), FALSE, 'Comentario de prueba 1', 1, 'Asunto 1', NOW(), 1),
+    (NOW(), FALSE, 'Comentario de prueba 2', 2, 'Asunto 2', NOW(), 2),
+    (NOW(), FALSE, 'Comentario de prueba 3', 1, 'Asunto 3', NOW(), 3),
+    (NOW(), FALSE, 'Comentario de prueba 4', 3, 'Asunto 4', NOW(), 4),
+    (NOW(), FALSE, 'Comentario de prueba 5', 2, 'Asunto 5', NOW(), 5),
+    (NOW(), FALSE, 'Comentario de prueba 6', 1, 'Asunto 6', NOW(), 6),
+    (NOW(), FALSE, 'Comentario de prueba 7', 3, 'Asunto 7', NOW(), 7),
+    (NOW(), FALSE, 'Comentario de prueba 8', 2, 'Asunto 8', NOW(), 8),
+    (NOW(), FALSE, 'Comentario de prueba 9', 1, 'Asunto 9', NOW(), 9),
+    (NOW(), FALSE, 'Comentario de prueba 10', 3, 'Asunto 10', NOW(), 1);
